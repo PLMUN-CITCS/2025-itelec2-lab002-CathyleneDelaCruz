81e8752aee1e972172a77f268b5133948333e3ca
BSIT Dela Cruz <cathydelacruz010@gmail.com>
2025-02-13 20:31:58 +0800

    Submitting Python Session 03 - Exercise 01

--- problem_02.py
+++ problem_02.py
@@ -1,13 +1,13 @@
-# Cathylene Fate Dela Cruz 
+# CathyleneDelaCruz
 # ITELEC2
-# Problem Set 01 - Problem 01
-# Simple Calculator Program
+# Problem Set 01 - Problem 02
+# Square the Number Program
 
 def main():
-	print("Square the Number Program")
-	number = int(input("Enter a number: "))
-	print(f"The square of {number} is {(number * number):.2f}")
-
-
+    print("Square the Number Program")
+    number = int(input("Enter a number: "))
+    print(f"The square of {number} is {(number * number):.2f}")
+    
+   
 if __name__ == "__main__":
     main()
